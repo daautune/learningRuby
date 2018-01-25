eb97be376ba097909b00cd2a6b0084e5df589fd4
maithao2206 <thaoptm2206@gmail.com>
2018-01-25 20:12:06 +0700

    change-name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def setting_title title
+    # if "#{title}".empty?
+    #   my_title = "beautiful | My pages"
+      # binding.pry
+      title = "ngo tien" if title.empty?
+    # else 
+      my_title = "#{title} | My pages"
+  end
+  def change_name
+    name = "Ten toi la: Tien Animal ahihi :)"
+    if current_page?(:controller => "static_pages", :action => "help")
+      result = ""
+      name.split("").reverse.each do |i|
+        result = result + "#{i}"
+      end
+      return result
+    elsif current_page?(:controller => "static_pages", :action => "about") 
+      return name
+    end
+  end
 end
+# end
